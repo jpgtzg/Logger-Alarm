ad48f7c9106456d85f7aad4b56ed0b965c573196
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-03-03 18:58:29 +0000

    feat: Add method to retrieve latest channel data

--- src/api/api_manager.py
+++ src/api/api_manager.py
@@ -5,7 +5,7 @@
 import dotenv
 import os
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 dotenv.load_dotenv()
 
@@ -14,7 +14,6 @@ api_key = os.getenv("API_KEY")
 
 def send_request(endpoint : str):
     url = base_url + endpoint + api_key
-    
     response = requests.get(url)
     response.raise_for_status()
 
@@ -41,3 +40,17 @@ def get_daily_data(serial_number : str, start_date : datetime, end_date : dateti
     start_date = start_date.strftime("%Y-%m-%d")
     end_date = end_date.strftime("%Y-%m-%d")
     return send_request(f"/Data/DailyStats/{serial_number}/{start_date}/{end_date}/")
+
+def get_latest_data(serial_number : str, channel_name : str):
+    start_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d %H:%M")
+    end_date = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    response = send_request(f"/Data/Channel/{serial_number}/{channel_name}/{start_date}/{end_date}/")
+    
+    if response and "data" in response[0] and response[0]["data"]:
+        all_data = response[0]["data"][0]["data"]
+        if all_data:
+            latest_entry = max(all_data, key=lambda x: x["timestamp"])
+            return latest_entry["timestamp"], latest_entry["value"]
+
+    return None
