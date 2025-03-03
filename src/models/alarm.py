0251f448968a437f1a51eca38559ee525408ae05
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-03-03 23:29:21 +0000

    refactor: Simplify Alarm class alarm triggering logic

--- src/models/alarm.py
+++ src/models/alarm.py
@@ -6,16 +6,14 @@
 """
 
 from models.alarm_type import AlarmType
-from datetime import datetime
+from datetime import datetime, timedelta
 from api.api_manager import get_latest_data
-
+    
 class Alarm:
     def __init__(self, serial_number: str, channel_name: str, alarm_type: AlarmType):
         self.serial_number = serial_number
         self.channel_name = channel_name
         self.alarm_type = alarm_type
-        self.alarm_triggered = False
-        self.alarm_triggered_time = None
 
     def __str__(self):
         return f"Alarm(serial_number={self.serial_number}, channel_name={self.channel_name}, alarm_type={self.alarm_type})"
@@ -24,17 +22,11 @@ class Alarm:
         data = get_latest_data(self.serial_number, self.channel_name)
 
         if data:
-            value = data[1]
+            timestamp, value = data
 
-            alarm_triggered = self.alarm_type.check_alarm(value)
+            if timestamp < datetime.now() - timedelta(hours=8):
+                return False
 
-            if alarm_triggered and not self.alarm_triggered:
-                self.alarm_triggered = True
-                self.alarm_triggered_time = datetime.now()
-
-            elif not alarm_triggered:
-                self.alarm_triggered = False
-
-            return alarm_triggered
+            return self.alarm_type.check_alarm(value)
 
         return False
