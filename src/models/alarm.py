52595f7e1677a2932679372f36260af3cd2f3a11
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-03-04 17:29:00 +0000

    refactor: Improve Alarm class data retrieval and alarm checking logic

--- src/models/alarm.py
+++ src/models/alarm.py
@@ -5,28 +5,30 @@
     This class is used to set alarms on specific loggers/channels
 """
 
-from models.alarm_type import AlarmType
 from datetime import datetime, timedelta
+from .alarm_type import AlarmType
 from api.api_manager import get_latest_data
     
 class Alarm:
-    def __init__(self, serial_number: str, channel_name: str, alarm_type: AlarmType):
+    def __init__(self, serial_number: str, channel: str, alarm_type: AlarmType):
         self.serial_number = serial_number
-        self.channel_name = channel_name
+        self.channel = channel
         self.alarm_type = alarm_type
 
     def __str__(self):
-        return f"Alarm(serial_number={self.serial_number}, channel_name={self.channel_name}, alarm_type={self.alarm_type})"
+        return f"Alarm(serial_number={self.serial_number}, channel_name={self.channel}, alarm_type={self.alarm_type})"
 
-    def check_alarm(self):
-        data = get_latest_data(self.serial_number, self.channel_name)
-
-        if data:
-            timestamp, value = data
-
-            if timestamp < datetime.now() - timedelta(hours=8):
-                return False
-
-            return self.alarm_type.check_alarm(value)
-
-        return False
+    def check_alarm(self) -> bool:
+        """
+        Check if the alarm should be triggered based on the latest data
+        """
+        # Get latest data from API
+        latest_data = get_latest_data(self.serial_number, self.channel)
+        
+        if latest_data:
+            value = latest_data[1]
+        
+            # Check value against thresholds
+            return self.alarm_type.check_alarm(float(value))
+        
+        return False  # Trigger alarm if no data available
